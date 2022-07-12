5f73aa38d0e9a02ade0a66838f9f82686c35dd83
teniolao <Tennybetty100@gmail.com>
2022-07-12 14:54:13 +0100

    create lesson detail component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./component/header/Header";
 import Home from "./component/home/Home";
 import Quiz from "./component/quiz/Quiz";
 import Lessons from "./component/lessons/Lessons";
+import LessonDetail from "./component/lessons/LessonDetail";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -34,6 +35,10 @@ function App() {
         <Route
           path="/lessons"
           element={<Lessons LessonsData={LessonsData} />}
+        />
+        <Route
+          path="/lessons/:title"
+          element={<LessonDetail LessonsData={LessonsData} />}
         />
       </Routes>
     </BrowserRouter>
---
+++ src/component/lessons/LessonDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LessonDetail = () => {
+  return (
+    <div>welcome to lesson detail</div>
+  )
+}
+
+export default LessonDetail
