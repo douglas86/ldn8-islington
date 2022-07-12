480d2b49dda8a45003b101ef96d6711d77fc1dcf
teniolao <Tennybetty100@gmail.com>
2022-07-12 14:32:19 +0100

    add lesson content with basic css

--- src/component/lessons/Card.js
+++ src/component/lessons/Card.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./Card.css";
 
 const Card = ({ LessonsData }) => {
-  return LessonsData.map((lesson) => (
-    <div className="card">
-      <div className="top">
+  return LessonsData.map((lesson, index) => (
+    <div key={index} className="card">
+      <div className="img-container">
+        <img className="circle-img" src={lesson.img_url} alt="avatar_img" />
+      </div>
+      <div className="text-container">
         <h2 className="name">{lesson.title}</h2>
+        {/* <p className="info">{lesson.intro}</p> */}
+
+        <div className="lesson-summary">{lesson.summary}</div>
+        {/* <Button variant="contained" style={{ margin: "0 20px" }}>
+        <Link to={`/lessons/${lesson.title}`}>View Lesson</Link>
+      </Button> */}
       </div>
     </div>
   ));
