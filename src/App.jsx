66b625078ec9b0697fa0692edb8c7f34bd039c6c
teniolao <Tennybetty100@gmail.com>
2022-07-20 14:52:33 +0100

    add video to lesson

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Lessons from "./component/lessons/Lessons";
 import LessonDetail from "./component/lessons/LessonDetail";
 import Post from "./component/home/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./app.css";
 
 const App = () => {
   return (
--- src/component/lessons/LessonDetail.js
+++ src/component/lessons/LessonDetail.js
@@ -7,13 +7,14 @@ import "./LessonDetail.css";
 const LessonDetail = ({ LessonsData }) => {
   const { id } = useParams();
   const [lessons, setLessons] = useState([]);
-
+ const base_url = "https://www.youtube.com/embed/";
+ 
   useEffect(() => {
     axios
       .get(`https://ldn8-islington.herokuapp.com/lessons/${id}`)
       .then((res) => setLessons(res.data));
   }, [id]);
-
+console.log(lessons)
   return lessons
     .filter((lesson) => lesson.id === parseInt(id))
     .map((lesson, index) => (
@@ -22,6 +23,13 @@ const LessonDetail = ({ LessonsData }) => {
         <div className="image-container">
           <img className="image" src={lesson?.img_url} alt="avatar_img" />
         </div>
+        <div className="frame">
+          <iframe
+            src={base_url + lesson?.video_url.slice(-11)}
+            title="YouTube video player"
+          ></iframe>
+        </div>
+
         <div className="lesson-content-container">
           <p>{lesson?.summary}</p>
         </div>
