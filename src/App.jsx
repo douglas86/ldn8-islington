2297c5e41790d7d900ac71ae41b555558e2d48b2
s-shah-coder <saf.h.shah@gmail.com>
2022-07-26 13:29:02 +0100

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import LessonDetail from "./component/lessons/LessonDetail";
 import Teacher from "./component/teacher/Teacher";
 import EditLesson from "./component/teacher/EditLesson";
 import Post from "./component/home/Post";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { withAuthenticationRequired, useAuth0 } from "@auth0/auth0-react";
+import { Route, Routes } from "react-router-dom";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 import "./App.css";
 
 const ProtectedRoute = ({ component, ...args }) => {
@@ -18,27 +18,23 @@ const ProtectedRoute = ({ component, ...args }) => {
 };
 
 const App = () => {
-  const { user, isAuthenticated } = useAuth0();
-  let isTeacher = false;
-  if (isAuthenticated) {
-    isTeacher = user["http://islington.com"]?.roles.includes("Teacher");
-  }
   return (
     <>
+      <Navbar />
       <Routes>
+        <Route
+          path="/teacher"
+          element={<ProtectedRoute component={Teacher} />}
+        />
         <Route
           path="/profile"
           element={<ProtectedRoute component={Profile} />}
         />
-      </Routes>
-
-      <Navbar />
-      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/questions/lessons/:id" element={<Quiz />} />
         <Route path="/lessons" element={<Lessons />} />
         <Route path="/lessons/:id" element={<LessonDetail />} />
-        {isTeacher && <Route path="/teacher" element={<Teacher />} />}
+        {/* <Route path="/teacher" element={<Teacher />} /> */}
         <Route path="/edit-lesson/:id" element={<EditLesson />} />
         <Route path="/post/:id" element={<Post />} />
       </Routes>
